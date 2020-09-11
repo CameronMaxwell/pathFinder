bb4d1c25dc2b5f22c45f5e671cb5bef7dfaf3ba1
Cameron Maxwell <66273331+CameronMaxwell@users.noreply.github.com>
2020-09-11 14:08:18 -0400

    Add files via upload

---
+++ OverflowException.java
@@ -0,0 +1,17 @@
+/**
+ * @author Cameron Maxwell
+ *
+ *  Represents the situation in which the stack is full
+ */
+
+public class OverflowException extends RuntimeException
+{
+  /**
+   * Sets up this exception with an appropriate message.
+   * @param message String describing the error that threw the exception
+   */
+  public OverflowException (String message)
+  {
+    super (message);
+  }
+}
